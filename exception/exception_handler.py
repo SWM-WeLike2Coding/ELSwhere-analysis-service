e2a2b4bfdfe5dd67160cb569ef3240e04e8d3d2a
kjungw1025 <kjungw1025@naver.com>
2024-09-05 16:00:09 +0900

    feat: 최초기준가격평가일의 날짜 예외처리 및 핸들러 추가

--- exception/exception_handler.py
+++ exception/exception_handler.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from urllib.request import Request
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
-from exception.errors import ProductServiceServerException
+from exception.errors import ProductServiceServerException, ValidateInitialBasePriceEvaluationDateException
 
 def add_exception_handler(app: FastAPI):
     @app.exception_handler(ProductServiceServerException)
@@ -17,5 +17,20 @@ def add_exception_handler(app: FastAPI):
                 "status": "INTERNAL_SERVER_ERROR",
                 "code": "ProductServiceServerException",
                 "message": "상품 서비스와 통신 중 문제가 발생하였습니다."
+            }
+        )
+
+    @app.exception_handler(ValidateInitialBasePriceEvaluationDateException)
+    async def validate_initial_base_price_evaluation_date_exception_handler(request: Request,
+                                                                            exc: ValidateInitialBasePriceEvaluationDateException):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content={
+                "timestamp": str(datetime.now()),
+                "trackingId": str(uuid.uuid4()),
+                "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
+                "status": "UNPROCESSABLE_ENTITY",
+                "code": "ValidateInitialBasePriceEvaluationDateException",
+                "message": "최초기준가격평가일이 계산하고자 하는 현재 시점 보다 미래의 날짜이므로 종가 데이터를 가져올 수 없습니다."
             }
         )
