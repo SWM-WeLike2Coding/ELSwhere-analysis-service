0941b6cb9a6d74bc0065cba4351dc89c2f4cbe6c
kjungw1025 <kjungw1025@naver.com>
2024-09-05 18:20:44 +0900

    test: 최초기준가격평가일이 현재 날짜를 넘어섰을 때 에러 처리가 잘 되는지에 대한 테스트 코드 작성

--- tests/api/routes/test_product.py
+++ tests/api/routes/test_product.py
@@ -61,3 +61,38 @@ async def test_get_price_ratio_failure_due_to_product_service(mock_do_service_as
     assert response_json["message"] == "상품 서비스와 통신 중 문제가 발생하였습니다."
     assert "timestamp" in response_json
     assert "trackingId" in response_json
+
+
+# 최초기준가격 대비 현재 기초자산가격 비율 - 최초기준가격평가일이 현재 날짜를 넘어섰을 때 에러 처리가 잘 되는지 확인
+@pytest.mark.asyncio
+async def test_get_price_ratio_failure_due_to_base_price_evaluation_date(mock_do_service_async,
+                                                                         mock_product_response,
+                                                                         mock_ticker):
+    # given
+    initial_date =(datetime.now().date() + timedelta(days=7)).strftime("%Y-%m-%d")
+    mock_product_response["initialBasePriceEvaluationDate"] = initial_date
+    mock_do_service_async.return_value = json.dumps(mock_product_response)
+
+    mock_ticker_instance = mock_ticker.return_value
+    mock_ticker_instance.history.side_effect = [
+        pd.DataFrame({"Close": [150.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [280.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [300.0]}, index=pd.to_datetime([initial_date])),
+
+        pd.DataFrame({"Close": [160.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [300.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [100.0]}, index=pd.to_datetime([initial_date]))
+    ]
+
+    # when
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response = await ac.get("/v1/product/price/ratio/1")
+
+    # then
+    assert response.status_code == 422
+    response_json = response.json()
+    assert response_json["status_code"] == 422
+    assert response_json["code"] == "ValidateInitialBasePriceEvaluationDateException"
+    assert response_json["message"] == "최초기준가격평가일이 계산하고자 하는 현재 시점 보다 미래의 날짜이므로 종가 데이터를 가져올 수 없습니다."
+    assert "timestamp" in response_json
+    assert "trackingId" in response_json
