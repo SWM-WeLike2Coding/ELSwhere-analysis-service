c05d92ee604de48b09be6e8da187fbc407a47740
kjungw1025 <kjungw1025@naver.com>
2024-09-05 18:15:54 +0900

    fix: 기존 최초기준가격 대비 현재 기초자산가격 비율에 대한 테스트 코드 수정

--- tests/api/routes/test_product.py
+++ tests/api/routes/test_product.py
@@ -1,6 +1,7 @@
 from httpx import AsyncClient, ASGITransport
 from exception.errors import ProductServiceServerException
 from main import app
+from datetime import datetime, timedelta
 import pytest
 import pandas as pd
 import json
@@ -8,23 +9,28 @@ import json
 # 최초기준가격 대비 현재 기초자산가격 비율 - 상품 API 호출이 성공할 때의 최초기준가격 대비 현재 기초자산가격 비율 테스트 케이스
 @pytest.mark.asyncio
 async def test_get_price_ratio_success(mock_do_service_async, mock_product_response, mock_ticker):
+    # given
+    initial_date =(datetime.now().date() - timedelta(days=7)).strftime("%Y-%m-%d")
+    mock_product_response["initialBasePriceEvaluationDate"] = initial_date
     mock_do_service_async.return_value = json.dumps(mock_product_response)
 
     mock_ticker_instance = mock_ticker.return_value
     # 요청에 따라 순서 대로 반환
     mock_ticker_instance.history.side_effect = [
-        pd.DataFrame({"Close": [150.0]}, index=pd.to_datetime(["2024-07-12"])),
-        pd.DataFrame({"Close": [280.0]}, index=pd.to_datetime(["2024-07-12"])),
-        pd.DataFrame({"Close": [300.0]}, index=pd.to_datetime(["2024-07-12"])),
+        pd.DataFrame({"Close": [150.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [280.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [300.0]}, index=pd.to_datetime([initial_date])),
 
-        pd.DataFrame({"Close": [160.0]}, index=pd.to_datetime(["2024-08-21"])),
-        pd.DataFrame({"Close": [300.0]}, index=pd.to_datetime(["2024-08-21"])),
-        pd.DataFrame({"Close": [100.0]}, index=pd.to_datetime(["2024-08-21"]))
+        pd.DataFrame({"Close": [160.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [300.0]}, index=pd.to_datetime([initial_date])),
+        pd.DataFrame({"Close": [100.0]}, index=pd.to_datetime([initial_date]))
     ]
 
+    # when
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/v1/product/price/ratio/1")
 
+    # then
     assert response.status_code == 200
     response_json = response.json()
     assert "initial" in response_json
@@ -37,14 +43,17 @@ async def test_get_price_ratio_success(mock_do_service_async, mock_product_respo
     assert response_json["recentAndInitialPriceRatio"] == -66.67
 
 
-# 최초기준가격 대비 현재 기초자산가격 비율 - 상품 API 호출이 실패할 때의 테스트 케이스
+# 최초기준가격 대비 현재 기초자산가격 비율 - 상품 API 호출이 실패할 때 에러 처리가 잘 되는지 확인
 @pytest.mark.asyncio
-async def test_get_price_ratio_failure(mock_do_service_async):
-    mock_do_service_async.side_effect = ProductServiceServerException(productIds=123)
+async def test_get_price_ratio_failure_due_to_product_service(mock_do_service_async):
+    # given
+    mock_do_service_async.side_effect = ProductServiceServerException(productIds=1)
 
+    # when
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/v1/product/price/ratio/1")
 
+    # then
     assert response.status_code == 500
     response_json = response.json()
     assert response_json["status_code"] == 500
