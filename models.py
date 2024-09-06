d392258d70af6211f16703ea66f318feff648002
kjungw1025 <kjungw1025@naver.com>
2024-09-06 23:46:56 +0900

    feat: 몬테카를로 결과 테이블 생성 및 필드 정의

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy.sql import func
+from core.database import Base
+from sqlalchemy import Column, BigInteger, DECIMAL, Text, DateTime
+
+
+class MonteCarloResult(Base):
+    __tablename__ = 'monte_carlo_result'
+
+    monte_carlo_result_id = Column(BigInteger, primary_key=True, autoincrement=True, comment="id")
+    product_id = Column(BigInteger, nullable=False)
+    early_repayment_probability = Column(Text, nullable=False)
+    maturity_repayment_probability = Column(DECIMAL(8,4), nullable=False)
+    loss_probability = Column(DECIMAL(8,4), nullable=False)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    last_modified_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
