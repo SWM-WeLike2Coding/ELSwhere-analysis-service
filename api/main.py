02330b6b6d9854ddad49b59885c1df0fe029b37e
kjungw1025 <kjungw1025@naver.com>
2024-09-09 10:13:00 +0900

    feat: 라우터 설정 추가

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from api.routes import product
+from api.routes import product, monte_carlo
 
 api_router = APIRouter()
 api_router.include_router(product.router, prefix="/v1/product", tags=["상품 분석"])
+api_router.include_router(monte_carlo.router, prefix="/v1/monte-carlo", tags=["몬테카를로 분석"])
