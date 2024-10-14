90d9a7d3516fa1f484905b09b930055b6ee76358
kjungw1025 <kjungw1025@naver.com>
2024-10-14 19:43:13 +0900

    feat: 상품에 대한 AI 결과 테이블 생성

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.sql import func
 from core.database import Base
-from sqlalchemy import Column, BigInteger, DECIMAL, Text, DateTime
+from sqlalchemy import Column, BigInteger, DECIMAL, Text, DateTime, Boolean
 
 
 class MonteCarloResult(Base):
@@ -19,6 +19,24 @@ class MonteCarloResultError(Base):
     __tablename__ = 'monte_carlo_result_error'
 
     monte_carlo_result_error_id = Column(BigInteger, primary_key=True, autoincrement=True, comment="id")
+    product_id = Column(BigInteger, nullable=False)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    last_modified_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+
+class AiResult(Base):
+    __tablename__ = 'ai_result'
+
+    ai_result_id = Column(BigInteger, primary_key=True, autoincrement=True, comment="id")
+    product_id = Column(BigInteger, nullable=False)
+    repayment_prediction = Column(Boolean, nullable=False)
+    accuracy = Column(DECIMAL(5,2), nullable=False)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    last_modified_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+
+class AiResultError(Base):
+    __tablename__ = 'ai_result_error'
+
+    ai_result_error_id = Column(BigInteger, primary_key=True, autoincrement=True, comment="id")
     product_id = Column(BigInteger, nullable=False)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     last_modified_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
