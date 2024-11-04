47023ceb5ece109a5aaf7fa43a283279a68524d1
kjungw1025 <kjungw1025@naver.com>
2024-11-04 18:23:24 +0900

    feat: 투자 성향에 맞는 상품 분석에 대한 api 라우터 추가

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from api.routes import product, monte_carlo, ai
+from api.routes import product, monte_carlo, ai, investment_propensity
 
 api_router = APIRouter()
 api_router.include_router(product.router, prefix="/v1/product", tags=["상품 분석"])
 api_router.include_router(monte_carlo.router, prefix="/v1/monte-carlo", tags=["몬테카를로 분석"])
 api_router.include_router(ai.router, prefix="/v1/ai", tags=["AI 분석"])
+api_router.include_router(investment_propensity.router, prefix="/v1/investment-propensity", tags=["투자 성향에 맞는 상품 분석"])
